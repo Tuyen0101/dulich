0eae4bd7f152b626f427d452078870c3e6a680df
nguyenvantuyen <tn650356@gmail.com>
2023-02-07 20:29:47 +0700

    update

--- travel-web/projects/admin/src/environments/environment.prod.ts
+++ travel-web/projects/admin/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://35.240.171.178:8000/'
+
 };
--- travel-web/projects/travel/src/environments/environment.prod.ts
+++ travel-web/projects/travel/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:8000/'
+  apiUrl: 'http://35.240.171.178:8000/'
 };
